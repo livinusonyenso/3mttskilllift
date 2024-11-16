85bad2642f6c879c7cc1778a4e6de3a6f7287b80
rolix202 <williamsroland147@gmail.com>
2024-11-16 22:41:41 +0100

    fixed issue on login api

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,15 @@
+import { validationResult } from "express-validator";
 import User from "../models/userModel.js";
 
 export const register = async (req, res, next) => {
     try {
+        const errors = validationResult(req)
+        
+        if (!errors.isEmpty()){
+            const errorMessage = errors.array().map((err) => err.msg)
+            return res.status(400).json({ errors: errorMessage});
+        }
+        
         const user = await User.create(req.body);
 
         if (!user) {
@@ -14,7 +22,6 @@ export const register = async (req, res, next) => {
         res.status(201).json({
             status: "success",
             message: "User successfully created!",
-            data: user
         });
     } catch (error) {
         console.error(error);
